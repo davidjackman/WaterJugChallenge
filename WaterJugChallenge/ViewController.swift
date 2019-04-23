b5e5aaecd5c17648ae6cf4668c887f7a8e1f9d1d
David Jackman <david@davidjackman.net>
2019-04-22 18:16:02 -0700

    Initial Commit

---
+++ WaterJugChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WaterJugChallenge
+//
+//  Created by David Jackman on 4/22/19.
+//  Copyright © 2019 David Jackman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
